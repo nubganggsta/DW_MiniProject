ef5dde304affd836fd6130019b24b9b7135d4b66
nanaddern <nanadda.r@kkumail.com>
2026-08-29 15:00:18 +0000

    add dim_cutomers & dim_trucks

---
+++ fiveGexpress_duckdb/models/fiveG_datawarehouse/dim_trucks.sql
@@ -0,0 +1,34 @@
+{{ config(
+    materialized='table'
+) }}
+
+with stg_trucks as (
+
+    select * 
+    from {{ ref('stg_trucks') }}
+
+)
+
+select
+    -- 1. Surrogate Key
+    md5(cast(truck_id as {{ dbt.type_string() }})) as truck_sk,
+
+    -- 2. Business Key
+    truck_id,
+
+    -- 3. Attributes
+    unit_number,
+    make,
+    model_year,
+    vin,
+    acquisition_date,
+    acquisition_mileage,
+    fuel_type,
+    tank_capacity,
+    status,
+    home_terminal,
+
+    -- 4. Audit Column
+    ingestion_timestamp as dim_created_at
+
+from stg_trucks
